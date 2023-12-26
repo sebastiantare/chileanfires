9bb5a9b5ba57e1da04e0d6547c0cc291ac38ab2b
sebastiantare <sebastiantarebustos@gmail.com>
2023-12-26 00:35:04 -0300

    minor changes in log path

--- setup-cron-jobs.sh
+++ setup-cron-jobs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Get the current username and groupname
 USERNAME=$(whoami)
 GROUPNAME=$(id -gn)
 CONDA_ENV_NAME="api"
@@ -9,10 +8,8 @@ APP_PATH="$HOME/chileanfires/fires-api/cron"
 GET_DATA="$APP_PATH/get-data.py"
 MERGE_DATA="$APP_PATH/merge-data.py"
 
-# Schedule cron jobs
 (crontab -l 2>/dev/null; echo "*/10 * * * * "$HOME/miniconda3/envs/$CONDA_ENV_NAME/bin/python" $GET_DATA") | crontab -
-(crontab -l 2>/dev/null; echo "*/10 * * * * "$HOME/miniconda3/envs/$CONDA_ENV_NAME/bin/python" $MERGE_DATA") | crontab -
+(crontab -l 2>/dev/null; echo "*/11 * * * * "$HOME/miniconda3/envs/$CONDA_ENV_NAME/bin/python" $MERGE_DATA") | crontab -
 
-# Set up scripts to run on boot (add to crontab)
 @reboot $HOME/miniconda3/envs/$CONDA_ENV_NAME/bin/python $GET_DATA
 @reboot $HOME/miniconda3/envs/$CONDA_ENV_NAME/bin/python $MERGE_DATA
